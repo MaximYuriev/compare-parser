27e5efff064f111b621f2f76e54d6559a59b222a
Maxim <yrev2011@gmail.com>
2025-05-13 01:39:42 +0700

    add if name == main construction

--- src/async_parser/main.py
+++ src/async_parser/main.py
@@ -1,3 +1,5 @@
+import asyncio
+
 import aiohttp
 
 from src.async_parser.db import get_async_engine, get_async_session_maker, save_bulletin_in_db
@@ -6,10 +8,14 @@ from src.config import config
 
 
 async def start_async_parser() -> None:
-    async_engine = get_async_engine(config.postgres.db_url)
+    async_engine = get_async_engine(config.postgres.db_url_async)
     session_maker = get_async_session_maker(async_engine)
 
     async with aiohttp.ClientSession() as session:
         bulletin_schema_list = await get_bulletin_schema_from_parsed_website(session)
 
     await save_bulletin_in_db(session_maker=session_maker, bulletin_schema_list=bulletin_schema_list)
+
+
+if __name__ == "__main__":
+    asyncio.run(start_async_parser())
